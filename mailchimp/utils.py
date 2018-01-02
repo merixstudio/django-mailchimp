1bd46da47efbba916efad22bb075641e572b9982
Mikolaj Jankowski <m.jankowski@merixstudio.com>
2018-01-02 13:28:54 +0100

    remove utils folder and add norwegian signs to utils.py

--- mailchimp/utils.py
+++ mailchimp/utils.py
@@ -438,3 +438,20 @@ def get_connection():
     if not CONNECTION.is_connected:
         CONNECTION.connect(API_KEY)
     return CONNECTION
+
+
+NORWEGIAN_TO_LATIN = {
+    u'Ø': 'O',
+    u'ø': 'o',
+    u'Å': 'A',
+    u'å': 'a',
+    u'Æ': 'A',
+    u'æ': 'a',
+    u'è': 'e',
+}
+
+
+def remove_norwegian_diacritics(s):
+    for norwegian, english in NORWEGIAN_TO_LATIN.items():
+        s = s.replace(norwegian, english)
+    return s
